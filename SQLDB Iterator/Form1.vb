cd5dbc9c356eb39ebc501608dbcb3cbc937c37ea
Marc <marcalfaro@live.com>
2019-06-09 10:57:39 +0800

    Added 3 queries as comments

--- SQLDB Iterator/Form1.vb
+++ SQLDB Iterator/Form1.vb
@@ -7,6 +7,12 @@ Public Class Form1
     End Sub
 
     Private Sub List_All_Database()
+        'There are 3 queries to do this:
+        '1. EXEC sp_databases
+        '2. SELECT name FROM master.sys.databases
+        '3. SELECT * FROM sys.databases d WHERE d.database_id > 4
+
+
         Using con As New SqlConnection("Data Source=MARCVX;Integrated Security=True")
             con.Open()
             Using cmd As New SqlCommand("sp_databases", con)
