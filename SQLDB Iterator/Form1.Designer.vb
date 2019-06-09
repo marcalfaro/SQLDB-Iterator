fdb3b97a14e54903d8ddf3562d9dbe3ac0c667d9
Marc <marcalfaro@live.com>
2019-06-09 10:49:44 +0800

    Add project files.

---
+++ SQLDB Iterator/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExecute = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'btnExecute
+        '
+        Me.btnExecute.Location = New System.Drawing.Point(46, 135)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(75, 23)
+        Me.btnExecute.TabIndex = 0
+        Me.btnExecute.Text = "Execute"
+        Me.btnExecute.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(34, 41)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(109, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "List all Databases"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(34, 64)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(90, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "List all Tables"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(632, 458)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.btnExecute)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SQL Server Iterator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExecute As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+End Class
---
+++ SQLDB Iterator/Form1.vb
@@ -0,0 +1,27 @@
+﻿Option Explicit On
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub btnExecute_Click(sender As Object, e As EventArgs) Handles btnExecute.Click
+        List_All_Database()
+    End Sub
+
+    Private Sub List_All_Database()
+        Using con As New SqlConnection("Data Source=MARCVX;Integrated Security=True")
+            con.Open()
+            Using cmd As New SqlCommand("sp_databases", con)
+                With cmd
+                    .CommandType = CommandType.StoredProcedure
+                    Using sReader As SqlDataReader = cmd.ExecuteReader
+                        If sReader.HasRows Then
+                            While sReader.Read
+                                Debug.Print(sReader("DATABASE_NAME"))
+                            End While
+                        End If
+                    End Using
+                End With
+            End Using
+            con.Close()
+        End Using
+    End Sub
+End Class
